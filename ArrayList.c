bf5978ebafeaec70f33e3f93a78ffca378b72f71
Max Base <maxbasecode@gmail.com>
2022-12-02 15:35:32 +0100

    create lib file and header

---
+++ ArrayList.c
@@ -0,0 +1,8 @@
+/*
+ * @Name: ArrayList C
+ * @Author: Max Base
+ * @Date: 2022/12/02
+ * @Repository: https://github.com/basemax/ArrayListC
+ */
+
+#include "ArrayList.h"
---
+++ ArrayList.h
@@ -0,0 +1,11 @@
+#ifndef _ARRAY_LIST_H_
+#define _ARRAY_LIST_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+#endif
+
